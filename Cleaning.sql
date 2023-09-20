9238a306624a63145e3ec749d048d8f915d24057
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2023-09-21 00:50:26 +0300

    Cleaning & Preprocessing

--- Cleaning.sql
+++ Cleaning.sql
@@ -18,12 +18,3 @@ ALTER TABLE pizza_sales ADD Order_Hour int;
 -- add data to column
 UPDATE pizza_sales SET Order_Hour = DATEPART(HOUR, order_time);
 
-
--- try to add order to see dashboard changed  or not
-INSERT INTO pizza_sales VALUES(1, 48621, 'hawaiian_mMD', 1, '2015-01-1', '11:38:36.0000000', 13.25, 13.25, 'Medium', 'Supreme',
-'Sliced Ham, Pineapple, Mozzarella Cheese', 'The Five Cheese Pizza')
-
-DELETE FROM pizza_sales WHERE order_id = 48621;
-
-
-SELECT * FROM pizza_sales
